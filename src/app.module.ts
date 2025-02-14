27bd3904bd44fe75c931b841f1cc36365b1286bd
liangyancheng <656040874@qq.com>
2025-02-14 15:05:36 +0800

    配置文件处理

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
+
+const envFilePath = `.env.${process.env.NODE_ENV || `development`}`;
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+      load: [() => dotenv.config({ path: '.env' })],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DB_PORT: Joi.number().default(3306),
+        DB_HOST: Joi.string().ip(),
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   getUsers(): any {
+    const dbPort = this.configService.get('DB_PORT');
+    console.log(dbPort);
     return this.userService.getUsers();
   }
 }
