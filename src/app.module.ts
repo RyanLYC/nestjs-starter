ba22e24a0881a0d8e527e8e6568273942f2ac1a6
liangyancheng <656040874@qq.com>
2025-02-18 15:52:36 +0800

    docker mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import * as dotenv from 'dotenv';
 import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const envFilePath = `.env.${process.env.NODE_ENV || `development`}`;
 
@@ -19,6 +20,18 @@ const envFilePath = `.env.${process.env.NODE_ENV || `development`}`;
         DB_PORT: Joi.number().default(3306),
         DB_HOST: Joi.string().ip(),
       }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '8.134.104.190',
+      port: 3310,
+      username: 'root',
+      password: 'example',
+      database: 'testdb',
+      entities: [],
+      // 同步本地的schema与数据库 -> 初始化的时候去使用
+      synchronize: true,
+      logging: ['error'],
     }),
     UserModule,
   ],
